f2b95532b12ce051cb314d7fd143c297471fcff3
ThandoHlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2023-02-22 01:35:13 +0200

    Create array_practice.php

---
+++ array_practice.php
@@ -0,0 +1,14 @@
+//just messing around with php:
+
+$country_list = ["South Africa", "namibia", "Zimbabwe"];
+$capital_city_and_no = ['Pretoria", 99, "windhoek", 77 "harare"]
+
+print_r($country_list);
+
+echo implode("\n.", $capital_city_and_no);
+
+$capital_and_country = [$country_list[2], $capital_city_and_no[1]];
+
+
+
+//will add more code:
